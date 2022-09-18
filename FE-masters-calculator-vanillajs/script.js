4df59d1ed29db3f534b1af9a0b43ecf23f6a803d
Jan Bock <j.g.bock@outlook.com>
2022-09-18 22:28:24 +0100

    adding frontend masters simple javascript calculator to past projects repo

---
+++ FE-masters-calculator-vanillajs/script.js
@@ -0,0 +1,87 @@
+let runningTotal = 0;
+let buffer = "0"; //space waiting for user input in type String
+let previousOperator = null; //previous operator will record what was previously press to be able to perform the calculation. This will not be set initially as there is no existing record of previous calculations upon launch
+const screen = document.querySelector(".calc_result");
+
+document
+  .querySelector(".calc_buttons")
+  .addEventListener("click", function (event) {
+    console.log(event.target.innerText);
+    buttonClick(event.target.innerText);
+  });
+
+function buttonClick(value) {
+  if (isNaN(parseInt(value))) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+  rerender();
+}
+
+function handleNumber(value) {
+  if (buffer === "0") {
+    buffer = value;
+  } else {
+    buffer += value;
+  }
+}
+
+function handleSymbol(value) {
+  switch (value) {
+    case "C":
+      buffer = "0";
+      runningTotal = 0;
+      previousOperator = null;
+      break;
+    case "=":
+      if (previousOperator === null) {
+        return;
+      }
+      flushOperation(parseInt(buffer));
+      previousOperator = null;
+      buffer = "" + runningTotal;
+      runningTotal = 0;
+      break;
+    case "BACK":
+      if (buffer.length === 1) {
+        buffer = "0";
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+    default:
+      //the default if none of the above cases fit
+      handleMath(value);
+      break;
+  }
+}
+
+function handleMath(value) {
+  const intBuffer = parseInt(buffer);
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
+
+  previousOperator = value;
+
+  buffer = "0";
+}
+
+function flushOperation(intBuffer) {
+  if (previousOperator === "+") {
+    runningTotal += intBuffer;
+  } else if (previousOperator === "-") {
+    runningTotal -= intBuffer;
+  } else if (previousOperator === "x") {
+    runningTotal *= intBuffer;
+  } else {
+    runningTotal /= intBuffer;
+  }
+}
+
+function rerender() {
+  screen.innerText = buffer;
+}
